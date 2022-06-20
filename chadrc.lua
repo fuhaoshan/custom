5fe3a3bcd6bb5a323d2d2d1b72bfc9fc7e8267e7
fuhao <851811250@qq.com>
2022-06-20 13:48:09 +0800

    打开了nvim-tree，打开文件时关闭的功能

--- chadrc.lua
+++ chadrc.lua
@@ -2,17 +2,28 @@
 
 local M = {}
 
+local override = require("custom.override")
 -- make sure you maintain the structure of `core/default_config.lua` here,
 -- example of changing theme:
 
 M.ui = {
-   theme = "onedark",
+  theme = "onedark",
 }
 
 M.options = {
   user = function ()
     require("custom.myoptions")
   end,
+}
+
+M.plugins = {
+
+  override = {
+    ["kyazdani42/nvim-tree.lua"] = override.nvimtree,
+  },
+
+  user = require("custom.plugins"),
+
 }
 
 return M
